f986f3b37aebaf17e71bf70241ebe80e65aec14b
melissasat <melissa.grach@live.fr>
2015-08-27 13:55:40 +0200

    update view

--- AnimalsReminder/LoadAnimalViewController.swift
+++ AnimalsReminder/LoadAnimalViewController.swift
@@ -15,9 +15,14 @@ class LoadAnimalViewController: UIViewController, UIPickerViewDelegate, UIPicker
     @IBOutlet weak var animalNameLabel: UILabel!
     @IBOutlet weak var animalAgeLabel: UILabel!
     @IBOutlet weak var animalTypeLabel: UILabel!
+    @IBOutlet weak var updateAnimalBtn: UIButton!
+    @IBOutlet weak var updateNameLabel: UITextField!
+    @IBOutlet weak var validateUpdate: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateNameLabel.alpha = 0
+        validateUpdate.hidden = true
         //saveInformation
         // Do any additional setup after loading the view.
     }
@@ -26,6 +31,8 @@ class LoadAnimalViewController: UIViewController, UIPickerViewDelegate, UIPicker
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    var intRes = 0
     
         //useLoadInPickerView
         func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
@@ -39,7 +46,7 @@ class LoadAnimalViewController: UIViewController, UIPickerViewDelegate, UIPicker
             var request = NSFetchRequest(entityName: "Animals")
             
             var results:NSArray = context.executeFetchRequest(request, error: nil)!
-
+            
             return results.count
        }
         
@@ -66,11 +73,36 @@ class LoadAnimalViewController: UIViewController, UIPickerViewDelegate, UIPicker
             var request = NSFetchRequest(entityName: "Animals")
             
             var results:NSArray = context.executeFetchRequest(request, error: nil)!
+            var name = results[row].name
             animalNameLabel.text = results[row].name
             animalTypeLabel.text = results[row].type
-
+            intRes = row;
+            
+            //UpdateBtn
+            updateAnimalBtn.setTitle("Modify" , forState: .Normal)
+            validateUpdate.hidden = false
         }
+    
+    @IBAction func UpdateOnClic(sender: AnyObject) {
+        updateNameLabel.alpha = 1
+        
+        println("update ask")
+    }
 
+    @IBAction func UpdateDone(sender: AnyObject) {
+        var appDel:AppDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
+        var context:NSManagedObjectContext = appDel.managedObjectContext!
+        
+        var request = NSFetchRequest(entityName: "Animals")
+        
+        var results:NSArray = context.executeFetchRequest(request, error: nil)!
+        var newName = updateNameLabel.text
+        println(updateNameLabel.text)
+        
+       /* var object:NSManagedObject = NSEntityDescription.insertNewObjectForEntityForName("Animals", inManagedObjectContext: context) as! NSManagedObject
+        object.setValue(newName, forKey: object[intRes].name)
+        context.save(nil)*/
+    }
     
     /*
     // MARK: - Navigation
