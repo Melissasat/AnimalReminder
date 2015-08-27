829118a6617f2f4c489fdf0e30803b771fc61943
melissasat <melissa.grach@live.fr>
2015-08-27 14:21:17 +0200

    Add Animal Protection

--- AnimalsReminder/AnimalViewController.swift
+++ AnimalsReminder/AnimalViewController.swift
@@ -52,35 +52,42 @@ class AnimalViewController: UIViewController, UIPickerViewDelegate, UIPickerView
     
     //SaveBtn
     @IBAction func SaveAnimalBtn(sender: AnyObject) {
-        //Add Birthday
-        var dateComponents = NSDateComponents()
-        dateComponents.day = animalDay.text.toInt()!
-        dateComponents.month = animalMonth.text.toInt()!
-        dateComponents.year = animalYear.text.toInt()!
-        
-        var calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
-        var birthdate = calendar.dateFromComponents(dateComponents)
-        var currentDate = NSDate()
-        
-        var getAge = calendar.components(NSCalendarUnit.CalendarUnitYear, fromDate: birthdate!, toDate: currentDate, options: nil)
-        var age = getAge.year
-        var type = typeChosen
-        
-        
-        //saveInformation
-        var appDel: AppDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
-        var context:NSManagedObjectContext = appDel.managedObjectContext!
-        
-        var newAnimal = NSEntityDescription.insertNewObjectForEntityForName("Animals",
-            inManagedObjectContext: context) as! NSManagedObject
-        
-        newAnimal.setValue(animalName.text, forKey: "name")
-        newAnimal.setValue(age, forKey: "age")
-        newAnimal.setValue(typeChosen, forKey: "type")
-        
-        context.save(nil)
-        
+        if (animalName.text.isEmpty || animalDay.text.isEmpty || animalMonth.text.isEmpty ||
+            animalYear.text.isEmpty){
+                
+                var alert = UIAlertController(title: "Alert", message: "Informations Missing", preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+
+        }else{
+            //Add Birthday
+            var dateComponents = NSDateComponents()
+            dateComponents.day = animalDay.text.toInt()!
+            dateComponents.month = animalMonth.text.toInt()!
+            dateComponents.year = animalYear.text.toInt()!
+            
+            var calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+            var birthdate = calendar.dateFromComponents(dateComponents)
+            var currentDate = NSDate()
+            
+            var getAge = calendar.components(NSCalendarUnit.CalendarUnitYear, fromDate: birthdate!, toDate: currentDate, options: nil)
+            var age = getAge.year
+            var type = typeChosen
+            
+            //saveInformation
+            var appDel: AppDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
+            var context:NSManagedObjectContext = appDel.managedObjectContext!
+            
+            var newAnimal = NSEntityDescription.insertNewObjectForEntityForName("Animals",
+                inManagedObjectContext: context) as! NSManagedObject
+            newAnimal.setValue(animalName.text, forKey: "name")
+            newAnimal.setValue(age, forKey: "age")
+            newAnimal.setValue(typeChosen, forKey: "type")
+            
+            context.save(nil)
+        }
     }
+    
      /*
     // MARK: - Navigation
 
